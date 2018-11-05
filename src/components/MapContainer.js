bebb652c8025930a0e877ad8d078e78784153ae0
James Oldeschulte <jamesoldeschulte@gmail.com>
2018-11-04 20:53:25 -0600

    Updates.

--- src/components/MapContainer.js
+++ src/components/MapContainer.js
@@ -12,41 +12,14 @@ export class MapContainer extends Component {
       showingInfoWindow: false,
       dogPic: {},
       clickCounter: 0,
-      message: "Failed to fetch from Unsplash =^.^="
+      message: "Failed to fetch from Unsplash =^.^=",
+      readyMap: 0
     };
   }
 
-  addClick = () => {
-    this.setState({clickCounter: this.state.clickCounter + 1});
-    if (this.state.clickCounter > 9) {
-      this.setState({clickCounter: 0});
-    }
-  }
-  fetchDogPic = () => fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=dog-park&orientation=landscape&client_id=8023829831c1fb39f95d040b7afc8643af3f4f66401f390da856195b78818556`)
-    .then(resp => {return resp.json() })
-    .then(data => {
-      //console.log(data.results)
-      return data.results[this.state.clickCounter].urls.small
-    })
-    .then(promiseUrl => {
-      this.setState({
-        dogPic: promiseUrl,
-        message: "Fetching dog pic from Unsplash"
-      })
-      //console.log('promiseUrl')
-      //console.log(promiseUrl)
-    })
-    .catch(
-      this.setState({
-        dogPic: this.props.venues[0].bestPhoto,
-        message: "Failed to fetch from Unsplash =^.^="
-      })
-    )
-
   componentDidMount() {
-    //console.log('component did mount')
+    //console.log(this.props.google.maps)
   }
-
 
   myBounds = () => {
     let bounds = new this.props.google.maps.LatLngBounds();
@@ -59,28 +32,20 @@ export class MapContainer extends Component {
 
   onMapReady = (mapProps, map) => {
     this.map = map;
-    // console.log(this.map);
+    //console.log(this.map);
     map.fitBounds(this.myBounds());
+    this.setState({readyMap: true});
+    console.log('ready')
   };
 
-  onMarkerClick = (props, marker, e) => {
-    this.setState({
-      selectedPlace: props,
-      activeMarker: marker,
-      showingInfoWindow: true,
-    });
-    this.fetchDogPic();
-    this.marker = marker;
-    this.addClick();
-    // console.log(this.marker.name);
-    // Try to fetch the foursqare stuff and store it
-  };
+  alertError = () => {
+    alert('Google Maps Error')
+  }
 
   render() {
 
     let bounds = this.myBounds()
     // console.log(bounds)
-
     return (
       <div className='map-container'>
         <Map
@@ -99,6 +64,12 @@ export class MapContainer extends Component {
           bounds={bounds}
           onReady={this.onMapReady}>
           {this.props.venues.map( v => {
+            let animate = this.props.google.maps.Animation.NONE
+            if (v.name === this.props.selectedPlace.name || this.props.venues.length === 1) {
+              animate = this.props.google.maps.Animation.DROP
+            }
+            //console.log(v.name);
+            //console.log(this.props.selectedPlace.name);
             return(<Marker
               className="marker"
               key={v.name}
@@ -106,21 +77,22 @@ export class MapContainer extends Component {
               address={v.location.address}
               bestPhoto={v.bestPhoto}
               position={{lat: v.location.lat, lng: v.location.lng }}
-              onClick={this.onMarkerClick}
-              onKeyPress={this.onMarkerClick}
+              onClick={this.props.onMarkerClick}
+              onKeyPress={this.props.onMarkerClick}
               tabIndex="0"
               icon={markerIcon}
+              animation={animate}
               />
             )
           })}
           <InfoWindow
-            marker={this.state.activeMarker}
-            visible={this.state.showingInfoWindow}>
+            marker={this.props.activeMarker}
+            visible={this.props.showingInfoWindow}>
               <div>
-                <h1>{this.state.selectedPlace.name}</h1>
-                <div className="park-pic"><img src={this.state.dogPic} alt={this.state.selectedPlace.name+" photo"}/></div>
-                <h3>{this.state.selectedPlace.address}</h3>
-                <p>{this.state.message}</p>
+                <h1>{this.props.activeMarker.name}</h1>
+                <div className="park-pic"><img src={this.props.dogPic} alt={this.props.selectedPlace.name+" photo"}/></div>
+                <h3>{this.props.selectedPlace.address}</h3>
+                <p>{this.props.message}</p>
               </div>
           </InfoWindow>
         </Map>
--- src/components/SearchableMap.js
+++ src/components/SearchableMap.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import MapContainer from './MapContainer'
 import {venues} from '../consts/dogparks'
 
-class SearchableApp extends Component {
+class SearchableMap extends Component {
   constructor(props) {
     super(props);
     this.state = {
@@ -11,9 +11,71 @@ class SearchableApp extends Component {
       searchButtonText: "Search Parks",
       searchListClass: "search",
       query: "",
-      selectedAddress: ""
+      selectedAddress: "",
+      selectedPlace: {},
+      activeMarker: {},
+      showingInfoWindow: false,
+      dogPic: {},
+      clickCounter: 0,
+      message: "Failed to fetch from Unsplash =^.^=",
+      allMarkers: [],
+      fakeWindowClass: "bottom hide"
     };
   }
+
+  addMarker = (m) => {
+    this.setState({
+      allMarkers: this.state.allMarkers + m
+    })
+  }
+
+  addClick = () => {
+    this.setState({clickCounter: this.state.clickCounter + 1});
+    if (this.state.clickCounter > 9) {
+      this.setState({clickCounter: 0});
+    }
+  }
+  fetchDogPic = () => fetch(`https://api.unsplash.com/search/photos/?page=1&per_page=10&query=dog-park&orientation=landscape&client_id=8023829831c1fb39f95d040b7afc8643af3f4f66401f390da856195b78818556`)
+    .then(resp => {return resp.json() })
+    .then(data => {
+      //console.log(data.results)
+      return data.results[~~(Math.random() * 10)].urls.small
+    })
+    .then(promiseUrl => {
+      this.setState({
+        dogPic: promiseUrl,
+        message: "Fetching dog pic from Unsplash"
+      })
+      //console.log('promiseUrl')
+      //console.log(promiseUrl)
+    })
+    .catch(
+      this.setState({
+        dogPic: this.state.venues[0].bestPhoto,
+        message: "Failed to fetch from Unsplash =^.^="
+      })
+    )
+
+  onMarkerClick = (props, marker, e) => {
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true,
+    });
+    // console.log('on marker click:');
+    // console.log(props);
+    // console.log(marker);
+    // console.log(e);
+    this.fetchDogPic();
+    this.marker = marker;
+    this.addClick();
+    if (this.state.fakeWindowClass === "bottom") {
+      this.setState({
+        fakeWindowClass: "bottom hide"
+      })
+    }
+  }
+
   searchClick = () => {
     if (this.state.searchButtonClass === "button open-search") {
       this.setState({
@@ -21,11 +83,23 @@ class SearchableApp extends Component {
         searchButtonText: "Search Parks",
         searchListClass: "search"
       })
+      //console.log('search click')
     } else {
       this.setState({
         searchButtonClass: "button open-search",
         searchButtonText: "X",
-        searchListClass: "search show"
+        searchListClass: "search show",
+        query: "",
+        selectedAddress: "",
+        selectedPlace: {},
+        activeMarker: {},
+        showingInfoWindow: false,
+        venues: venues
+      })
+    }
+    if (this.state.fakeWindowClass === "bottom") {
+      this.setState({
+        fakeWindowClass: "bottom hide"
       })
     }
   }
@@ -35,7 +109,7 @@ class SearchableApp extends Component {
     this.setState({
       venues: filtered_venues
     })
-    if (filtered_venues.length == 1) {
+    if (filtered_venues.length === 1) {
       this.setState({
         selectedAddress: address
       })
@@ -52,16 +126,20 @@ class SearchableApp extends Component {
     this.filterVenues(e.target.value)
   }
   parkClick = (name, address) => {
-    console.log(name)
-    this.setState({
-      query: name
-    })
+    //console.log(name)
     this.filterVenues(name, address)
+    //console.log(venues[0])
+    this.searchClick();
+    this.fetchDogPic();
+    this.setState({
+      query: name,
+      fakeWindowClass: "bottom"
+    })
   }
 
   render() {
     return (
-      <div className="SearchableApp">
+      <div className="SearchableMap">
         <div
           className={this.state.searchButtonClass}
           role='button'
@@ -92,19 +170,32 @@ class SearchableApp extends Component {
                 onKeyPress={() => this.parkClick(v.name, v.location.address)}
                 id={v.name}
                 tabIndex={"0"}
+                aria-describedby={"Select " + v.name}
               >{v.name}</li>
               )
             }
           </ul>
-          <div
-            className="bottom">
-            <h3>{this.state.selectedAddress}</h3>
-          </div>
         </div>
-        <MapContainer venues={this.state.venues}/>
+        <div className={this.state.fakeWindowClass}>
+        <h1>{this.state.venues[0].name}</h1>
+        <div className="small-park-pic"><img src={this.state.dogPic} alt={this.state.venues[0].name+" photo"}/></div>
+        <h3>{this.state.venues[0].location.address}</h3>
+        <p>{this.state.message}</p>
+        </div>
+        <MapContainer
+          venues={this.state.venues}
+          onMarkerClick={this.onMarkerClick}
+          selectedPlace={this.state.selectedPlace}
+          activeMarker={this.state.activeMarker}
+          showingInfoWindow={this.state.showingInfoWindow}
+          dogPic={this.state.dogPic}
+          clickCounter={this.state.clickCounter}
+          message={this.state.message}
+          addMarker={this.addMarker}
+        />
       </div>
     );
   }
 }
 
-export default SearchableApp;
+export default SearchableMap;
